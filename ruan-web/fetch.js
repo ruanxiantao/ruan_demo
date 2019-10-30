673a284423b24418b151ad6b8e26eff364855487
阮先涛 <2350844471@qq.com>
2019-10-30 19:01:31 +0800

    fetch.js文件

---
+++ ruan-web/fetch.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import {Message} from 'element-ui';
+
+const service = axios.create({
+    baseURL = '',
+    timeout:60000
+});
+
+service.interceptors.request.use((config) => {
+    config.headers['dajiaId'] = 'aaa';
+    return config;
+},(err) => {
+    return Promise.reject(err);
+}),
+
+service.interceptors.response.use(
+    response => {
+        return response.data;
+},error => {
+        Message({
+            message:'网络请求异常',
+            type:error,
+            duration:3 * 1000
+        });
+        return Promise.reject(error);
+}
+)
